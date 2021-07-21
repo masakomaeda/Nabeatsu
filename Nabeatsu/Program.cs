6f030acdd4ccc0c36933ebfe993211a6468ecf1a
masakomaeda <masakomaeda126@yahoo.co.jp>
2021-07-22 07:20:10 +0900

    プロジェクト ファイルを追加します。

---
+++ Nabeatsu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SMax.NewCore21.MorningProject.Nabeatsu
+{
+    class Program
+    {
+        /// <summary>
+        /// メイン
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //*** ユーザーに値の入力を促す
+            Console.WriteLine("１からＮまでの数値を世界的な表現で表します");
+            Console.Write("Ｎ（数値）＝？");
+            var inputStr = Console.ReadLine();
+
+            //*** 文字、負数は処理対象外とする。
+            if (!int.TryParse(inputStr,out int inputNum))
+            {
+                Console.WriteLine("あなたは日本レベルです、さようなら");
+            }
+            else if (inputNum <= 0)
+            {
+                Console.WriteLine("あなたアジアレベルです、さようなら");
+            }
+            else
+            {
+                var normalNum = Enumerable.Range(1, inputNum);
+                var woldTransNum = normalNum.Select(n => new WorldTrans(n).ToString());
+                var outputStr = string.Join(",", woldTransNum);
+
+                //*** 結果を出力して終了
+                Console.WriteLine(outputStr);
+            }
+        }
+    }
+}
---
+++ Nabeatsu/WorldTrans.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMax.NewCore21.MorningProject.Nabeatsu
+{
+    /// <summary>
+    /// 与えられた数値を世界的な表現に変換するクラス
+    /// </summary>
+    public sealed class WorldTrans 
+    {
+        #region    定数
+        private const int    KEYCODE = 3;
+        private const string KEYWORD = "aho";
+        #endregion 定数
+
+        #region    プライベートメンバ
+        private int _value;
+        #endregion プライベートメンバ
+
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="value">変換処理対象となる数字</param>
+        public WorldTrans(int value)
+        {
+            _value = value;
+        }
+
+        /// <summary>
+        /// ToStringのオーバーライド
+        /// </summary>
+        /// <returns>コンストラクタで指定された数値を変換処理した結果</returns>
+        public override string ToString()
+        {
+            return ChangeToWorldNum();
+        }
+
+        #region    プライベートメソッド
+        /// <summary>
+        /// 変換処理
+        /// </summary>
+        /// <returns></returns>
+        private string ChangeToWorldNum()
+        {
+            var ret = _value.ToString();
+
+            // 3(KEYCODE)の倍数かそれが含まれるときはaho(KEYWORD)を返す
+            if (_value % KEYCODE == 0)
+            {
+                ret = KEYWORD;
+            }
+            else if (ret.Contains(KEYCODE.ToString()))
+            {
+                ret = KEYWORD;
+            }
+
+            return ret;
+        }
+        #endregion プライベートメソッド
+
+    }
+}
